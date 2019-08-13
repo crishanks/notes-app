9653e91fed92caff2b40c6d2a3b75af31cc86313
github username <crishanks12@gmail.com>
2019-08-12 19:36:24 -0600

    appendFileSync

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 const fs = require('fs');
 
-fs.writeFileSync('notes.txt', 'This file was created by Node.js!');
+fs.writeFileSync('notes.txt', 'My name is Cris.');
+
+fs.appendFileSync('notes.txt', " I'm a computer genius!");
