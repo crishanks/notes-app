b999c2b510d90413884a7c7a20b3a04159fb1cea
github username <crishanks12@gmail.com>
2019-08-19 17:11:09 -0600

    add validator module

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
+const validator = require('validator');
 const getNotes = require('./notes');
 
 const message = getNotes();
 console.log(message);
+
+console.log(validator.isURL('https://google.com'));
