2ed40bba915a618d924cd4ebbf56c436c3f5a019
github username <crishanks12@gmail.com>
2019-08-19 17:18:33 -0600

    chalk package

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const validator = require('validator');
+const chalk = require('chalk');
 const getNotes = require('./notes');
 
 const message = getNotes();
 console.log(message);
 
-console.log(validator.isURL('https://google.com'));
+console.log(chalk.bold.bgGreen.black('Success!'));
