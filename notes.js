5b697bcf06672cccf0ef8e4929e1e3ca577704db
github username <crishanks12@gmail.com>
2019-08-19 20:48:51 -0600

    add a note functionality

--- notes.js
+++ notes.js
@@ -1,5 +1,37 @@
+const fs = require('fs')
+
 const getNotes = () => {
   return "Your notes...";
 }
 
-module.exports = getNotes;
+const addNote = function(title, body) {
+  const notes = loadNotes()
+  const duplicateNotes = notes.filter(note => note.title !== title)
+
+  duplicateNotes.push({
+    title: title,
+    body: body
+  })
+  
+  saveNotes(duplicateNotes)
+}
+
+const saveNotes = function(notes) {
+  const dataJSON = JSON.stringify(notes)
+  fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = function() {
+  try {
+    const dataBuffer = fs.readFileSync('notes.json')
+    const dataJSON = dataBuffer.toString()
+    return JSON.parse(dataJSON)
+  } catch(e) {
+    return []
+  }
+}
+
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote
+}
